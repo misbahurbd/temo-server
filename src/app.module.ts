4e50c5369977d85269cd641bcad6264326cc1be8
Misbahur Rahman <misbahurbd@gmail.com>
2025-11-18 09:28:52 +0600

    fix auth import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-// import { PrismaModule } from './infrastructure/prisma/prisma.module';
+import { PrismaModule } from './infrastructure/prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { RedisModule } from './infrastructure/redis/redis.module';
 import { validateEnv } from './validations/env.validation';
-import { AuthModule } from './auth/auth.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, validate: validateEnv }),
+    PrismaModule,
     RedisModule,
     AuthModule,
   ],
