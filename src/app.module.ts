96dc57cb610b07292ab79c1517a066a31e78f63d
Misbahur Rahman <misbahurbd@gmail.com>
2025-11-18 09:24:40 +0600

    configure session with passport initialize

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { RedisModule } from './infrastructure/redis/redis.module';
 import { validateEnv } from './validations/env.validation';
+import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, validate: validateEnv }),
     RedisModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
